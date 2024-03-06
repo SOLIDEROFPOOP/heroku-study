94a2cd3c20cd995beac3e3897a3deba583c48884
Kapparov Murat <136073953+SOLIDEROFPOOP@users.noreply.github.com>
2024-03-06 16:39:47 +0600

    Update Controller.java

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class Controller {
     @GetMapping
     public String hellop(){
         return "HELLO";
+    }
+    @GetMapping("/test")
+    public String bruh(@RequestAttribute("xd") String xd){
+        return xd;
     }
 }
